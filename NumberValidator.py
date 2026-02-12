9d56cbd1affd607891ea9828dd0e6390b19feada
Egert <egert.koppel@gmail.com>
2026-02-12 22:31:28 +0200

    removed unnecessary comments

--- NumberValidator.py
+++ NumberValidator.py
@@ -1,36 +1,6 @@
-
-# Copilot generated a class based on this not sure if it works correctly
-# def attach_number_validator(self, line_edit: QLineEdit):
-#         validator = QDoubleValidator() 
-#         validator.setNotation(QDoubleValidator.StandardNotation) 
-#         line_edit.setValidator(validator) 
-
-#     # They work as spacers and if you click enter they are automatically removeed, it considers only the dot as real comma
-#     # Not sure how big of a problem it is but allows inserting multiple "," every other check works (for example "1,1,1,1" is accepted)
-#     def attach_number_tooltip(self, line_edit: QLineEdit): 
-#         self.attach_number_validator(line_edit)
-    
-#         def event_filter(obj, event): 
-#             if obj is line_edit and event.type() == QEvent.KeyPress: 
-#                 before = line_edit.text() 
-#                 result = QLineEdit.event(obj, event) 
-#                 after = line_edit.text()
-#                 if before == after and not event.text().isnumeric(): 
-#                     QToolTip.showText( line_edit.mapToGlobal(QPoint(0, line_edit.height())), "Numbers only" ) 
-#                     return result 
-#                 elif after.count(",") + after.count(".") > 1: 
-#                     QToolTip.showText( line_edit.mapToGlobal(QPoint(0, line_edit.height())), "Only one comma allowed" ) 
-#                     line_edit.setText(before)
-#                 return True 
-    
-#         line_edit.installEventFilter(line_edit) 
-#         line_edit.eventFilter = event_filter
-
-
 from PySide6.QtWidgets import QLineEdit, QToolTip
 from PySide6.QtGui import QDoubleValidator
 from PySide6.QtCore import QEvent, QPoint
-
 
 class NumericInput(QLineEdit):
     def __init__(self, parent=None):
