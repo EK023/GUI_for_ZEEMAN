c8c8b462b068795b0e04fc9ad2db75af37dbf294
Egert <egert.koppel@gmail.com>
2026-02-21 21:51:08 +0200

    first prototype of selecting a range from plot

--- my_app.py
+++ my_app.py
@@ -4,11 +4,11 @@ from PySide6.QtCore import (
     Qt, 
     QRegularExpression as QRegExp, # check if it's needed, no need for numbers but chem elems might need it
     QEvent,
-    QPoint,
 )
 from PySide6.QtWidgets import (
     QApplication, 
-    QVBoxLayout, 
+    QVBoxLayout,
+    QLineEdit,
 )
 import pyqtgraph as pg
 import numpy as np
@@ -18,9 +18,19 @@ from SimpleFieldGroup import SimpleFieldGroup
 from ElementFieldGroup import ElementFieldGroup
 
 import matplotlib.pyplot as plt
+from matplotlib.widgets import SpanSelector
+
+from matplotlib.backends.backend_qtagg import FigureCanvas, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 
 uiclass, baseclass = pg.Qt.loadUiType("plot.ui")
 
+class MplCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        super().__init__(self.fig)
 
     
 class MainWindow(uiclass, baseclass):
@@ -29,10 +39,45 @@ class MainWindow(uiclass, baseclass):
         self.setupUi(self)
         # Might need to use some external plotting tool and insert it 
         # here before plotting, e.g., matplotlib or pyqtgraph
-
+        self.plot_widget = QVBoxLayout(self.widget)
         # In  the future let the user choose the data file for plotting
         data = np.loadtxt("plot1", usecols=(0, 1))
-        self.widget.plot(data[:, 0], data[:, 1])
+
+        self.graph_ranges = QVBoxLayout(self.scrollAreaWidgetContents)
+        # from https://matplotlib.org/stable/gallery/widgets/span_selector.html
+        
+
+        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
+        x = data[:, 0]
+        y = data[:, 1]
+        self.x = x
+        self.y = y
+
+        self.sc.axes.plot(x, y)
+        self.sc.axes.set_xlim(x.min(), x.max())
+        self.sc.axes.set_ylim(y.min(), y.max())
+        self.plot_widget.addWidget(self.sc)
+        self.plot_widget.addWidget(NavigationToolbar(self.sc, self))
+
+        def onselect(xmin, xmax):
+            indmin, indmax = np.searchsorted(self.x, (xmin, xmax))
+            indmax = min(len(self.x) - 1, indmax)
+
+            region_x = self.x[indmin:indmax]
+
+            if len(region_x) >= 2:
+                self.graph_ranges.addWidget(QLineEdit(f"Selected range: {region_x[0]:.2f} - {region_x[-1]:.2f}"))
+
+        self.span = SpanSelector(
+            self.sc.axes,
+            onselect,
+            "horizontal",
+            useblit=True,
+            props=dict(alpha=0.5, facecolor="tab:blue"), # can change color 
+            interactive=True,
+            drag_from_anywhere=True
+        )
+
         
         self.scroll_layout = QVBoxLayout(self.tab2_scrollAreaWidgetContents) # inside a scroll element
         self.scroll_layout2 = QVBoxLayout(self.simpleParams)
@@ -64,11 +109,8 @@ class MainWindow(uiclass, baseclass):
 
     def collect_values(self):
         results = {}
-
         for name, field in self.fields.items():
-            results[name] = field.get()
-
-        
+            results[name] = field.get()    
         for i, element in enumerate(self.elements):
             results[f"element_{i}"] = element.get()
 
@@ -83,30 +125,9 @@ class MainWindow(uiclass, baseclass):
 
     def add_element_widget(self, element):
         self.scroll_layout.takeAt(self.scroll_layout.count()-1)
-
-        # elementLayout = QHBoxLayout(elementWidget)
-
-        # el = QLineEdit()
-        # est = NumericInput()
-        # fit = QCheckBox()
-        # iter = QCheckBox()
-
-        # self.add_elements_to_layout(elementLayout, [el, est, fit, iter])
-        
-        # # changes in fields are also saved to elements object, makes it easier to deal with them later
-        # el.textChanged.connect(lambda text, e=element: setattr(e, "element", text)) 
-        # est.textChanged.connect(lambda text, e=element: setattr(e, "estimate", float(text.replace(",", ".")))) 
-        # fit.stateChanged.connect(lambda state, e=element: setattr(e, "fit", bool(state))) 
-        # iter.stateChanged.connect(lambda state, e=element: setattr(e, "iterlist", bool(state))) 
-
-
-        # self.elementWidgets.append((el, est, fit, iter))
         group = ElementFieldGroup(element)
         self.scroll_layout.addWidget(group)
         self.scroll_layout.addStretch()
-        # print(self.elementWidgets[0][0].text())      # How to access first element's name
-        # print(self.elementWidgets[0][2].isChecked()) # How to access first element's fit checkbox
-
     
     def add_elements_to_layout(self, layout, elements):
         for e in elements:
